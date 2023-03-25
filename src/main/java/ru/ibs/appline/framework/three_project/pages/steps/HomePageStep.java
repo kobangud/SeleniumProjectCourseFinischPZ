0cd3bbc77fe842378c6413b9487f61eedb98492c
kobangud <kobangud@mail.ru>
2023-03-25 15:16:58 +0300

    Финальное задание

---
+++ src/main/java/ru/ibs/appline/framework/three_project/pages/steps/HomePageStep.java
@@ -0,0 +1,36 @@
+package ru.ibs.appline.framework.three_project.pages.steps;
+
+import io.cucumber.java.bg.И;
+import ru.ibs.appline.framework.three_project.pages.managere.PageManager;
+
+public class HomePageStep {
+
+    PageManager pageManager = PageManager.getInstance();
+
+
+    @И("^Проверяем загрузку домашней страницы$")
+    public void loadingPage() {
+        pageManager.getHomePage().loadingPage();
+    }
+
+
+    //Жмем меню каталог
+    @И("^Жмем меню каталог$")
+    public void catalogMenu() {
+        pageManager.getHomePage().catalogMenu();
+    }
+
+    //В меню Каталог - выбираем Вкладку
+    @И("^В меню Каталог - выбираем Вкладку '(.+)'$")
+    public void selectCatalogMenuByText(String nameMenu) {
+        pageManager.getHomePage().selectCatalogMenuByText(nameMenu);
+    }
+
+
+    //В меню Каталог - выбираем подменю
+    @И("^В меню Каталог - выбираем подменю  '(.+)'$")
+    public void selectCatalogSubMenuByText(String nameMenu) {
+        pageManager.getHomePage().selectCatalogSubMenuByText(nameMenu);
+    }
+
+}
